f9551a5bc44c8a9f9a552236d0677c7b6a8fdfbc
naka_t <naka.t06+atlassian@gmail.com>
2018-12-12 17:10:33 +0900

    import文も含めるよう変更

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import os
+
 # mnistダウンロード
 mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
 
